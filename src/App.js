f5f2923f4ed1b1fa8d945204d05e09b970e4811d
Shi-330 <yshi95519@gmail.com>
2025-01-30 23:13:06 +0900

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import InterviewAnalyzer from './components/InterviewAnalyzer';
+
+function App() {
+  return (
+    <div className="App">
+      <InterviewAnalyzer />
+    </div>
+  );
+}
+
+export default App;
